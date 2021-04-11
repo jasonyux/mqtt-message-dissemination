1e9c99e379f7b5ba89b97b8e8c9d3f693b36d3b4
jasonyux <jasonyux16@gmail.com>
2021-04-11 22:49:39 +0800

    heat_map init with 7/7 Topic Hit Rate

--- containers/exp_clients.sh
+++ containers/exp_clients.sh
@@ -7,7 +7,7 @@ PUB_PORT=8088
 
 DIRECTORY=$3
 PUB_WAIT=$4
-TOPIC=a7
+TOPICS=a7
 
 if [ -z "$SUB_IP" ] || [ -z "$PUB_IP" ] || [ -z "$DIRECTORY" ]; then
 	echo "usage: ./exp_clients.sh <sub_broker_ip> <pub_broker_ip> <path> <pub_wait>"
@@ -18,9 +18,8 @@ if [ -z "$PUB_WAIT" ]; then
 	PUB_WAIT=5
 fi
 
-SUB_COMMAND="${DIRECTORY}/subscriber_args ${SUB_IP} ${SUB_PORT} 1 ${TOPIC}"
-PUB_COMMAND="${DIRECTORY}/publisher_args ${PUB_IP} ${PUB_PORT} ${TOPIC}"
-
+SUB_COMMAND="${DIRECTORY}/subscriber_args ${SUB_IP} ${SUB_PORT} 7 a1 a2 a3 a4 a5 a6 a7"
+PUB_COMMAND="${DIRECTORY}/publisher_args ${PUB_IP} ${PUB_PORT} 7 a1 a2 a3 a4 a5 a6 a7"
 
 eval "docker exec -d clients_sub_1 sh -c '${SUB_COMMAND}'"
 echo "[ LOG ] executed docker exec -d clients_sub_1 sh -c '${SUB_COMMAND}'"
