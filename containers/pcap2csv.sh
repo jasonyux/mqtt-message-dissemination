2dbcb7fa0527733468f80c6479543f79af60e0c1
jasonyux <jasonyux16@gmail.com>
2021-04-25 18:17:54 +0800

    updated pcap2csv.sh file for mqtt specific information

--- containers/pcap2csv.sh
+++ containers/pcap2csv.sh
@@ -2,16 +2,25 @@
 
 COUNT=1
 PATH=$1
-PARAMS="-T fields -e frame.time_relative -e frame.len -e ip.src -e ip.dst -E header=y -E separator=,"
+PARAMS="-T fields -e frame.time_relative -e frame.len -e ip.src -e ip.dst \
+	-e mqtt.msgtype -e mqtt.qos -e mqtt.topic -e mqtt.msg -e mqtt.msgid \
+	-E header=y -E separator=, -E aggregator=^ \
+	-d tcp.port==8088,mqtt"
 
 if [ -z "$PATH" ]; then
 	echo "usage: ./pcap2csv.sh <path>"
 	exit
 fi
 
+if [ -d "${PATH}/csv" ]; then
+	echo "[ ERROR ] ${PATH}/csv already exists"
+	exit
+fi
+
 eval "/usr/bin/mkdir ${PATH}/csv"
 
 while [ $COUNT -le 7 ]; do
-	eval "/usr/bin/tshark -r ${PATH}/b${COUNT}/b${COUNT}.pcap $PARAMS > ${PATH}/csv/b${COUNT}.csv"
+	eval "/usr/bin/tshark -r ${PATH}/b${COUNT}/b${COUNT}.pcap $PARAMS > \ 
+		${PATH}/csv/b${COUNT}.csv"
 	COUNT=$(($COUNT + 1))
 done
