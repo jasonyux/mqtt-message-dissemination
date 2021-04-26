207669a494b5f1ff6b2e3c4d299415d5baa6b629
jasonyux <jasonyux16@gmail.com>
2021-04-26 21:49:02 +0800

    [ heat map 1 ] last update

--- containers/exp_clients.sh
+++ containers/exp_clients.sh
@@ -18,8 +18,8 @@ if [ -z "$PUB_WAIT" ]; then
 	PUB_WAIT=5
 fi
 
-SUB_COMMAND="${DIRECTORY}/subscriber_args ${SUB_IP} ${SUB_PORT} 7 a1 a2 a3 a4 a5 a6 a7"
-PUB_COMMAND="${DIRECTORY}/publisher_args ${PUB_IP} ${PUB_PORT} 100 1000 1 a1"
+SUB_COMMAND="${DIRECTORY}/subscriber_args ${SUB_IP} ${SUB_PORT} 7"
+PUB_COMMAND="${DIRECTORY}/publisher_args ${PUB_IP} ${PUB_PORT} 100 1000 7"
 
 #DOC_SUB_COMMAND="docker exec -d clients_sub_1 sh -c '${SUB_COMMAND}'"
 
